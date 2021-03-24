051951b5676418dca352200f784616a8d26df430
Kaname <inmortaldragonxspace@outlook.com>
2021-03-23 21:43:50 -0600

    * 🔧 Fixed typo in the method to destroy a WS connection. * ➕ Added descriptions to methods and listeners.

--- index.d.ts
+++ index.d.ts
@@ -1,6 +1,6 @@
-import EventEmitter from 'events';
+import EventEmitter from "events";
 
-declare module 'harusame' {
+declare module "harusame" {
   interface SongUpdateData {
     songName: string;
     songArtist: string;
@@ -35,19 +35,74 @@ declare module 'harusame' {
   }
 
   export class Harusame extends EventEmitter {
+    /**
+     * Options for connecting Harasume to Listen.moe
+     * @param config Client connection options
+     */
     constructor(config?: Config);
 
+    /**
+     * Connect JPOP / KPOP LISTEN.moe WS
+     * @param radio JP or KR - Case sensitive
+     */
     public connect(radio: string): void;
-    public destroy(): void;
 
+    /**
+     * Destroy JPOP / KPOP LISTEN.moe WS
+     * @param radio JP or KR - Case sensitive
+     */
+    public destroy(radio: string): void;
+
+    /**
+     * Get current client configuration
+     */
     get config(): Config;
+
+    /**
+     * Get current KR and JP type songs from Listen.moe
+     */
     get song(): Song;
 
-    on(event: 'debug', listener: (name: string, msg: string) => void): this;
-    on(event: 'error', listener: (name: string, error: string) => void): this;
-    on(event: 'close', listener: (name: string, reason: string) => void): this;
-    on(event: 'open', listener: (name: string) => void): this;
-    on(event: 'ready', listener: (name: string) => void): this;
-    on(event: 'songUpdate', listener: (name: string, data: SongUpdateData) => void): this;
+    /**
+     * Emitted when a debug message is fired.
+     * @param event Debug
+     * @param listener Name Socket and msg data
+     */
+    on(event: "debug", listener: (name: string, msg: string) => void): this;
+
+    /**
+     * Emitted when an error was thrown when handling something. must be handled
+     * @param event Error
+     * @param listener Name Socket and reason data
+     */
+    on(event: "error", listener: (name: string, error: string) => void): this;
+
+    /**
+     * Emitted when a websocket connection closed.
+     * @param event Close
+     * @param listener Name Socket and reason data
+     */
+    on(event: "close", listener: (name: string, reason: string) => void): this;
+
+    /**
+     * Emitted when a websocket connection is opened.
+     * @param event Open
+     * @param listener Name Socket data
+     */
+    on(event: "open", listener: (name: string) => void): this;
+
+    /**
+     * Emitted when a websocket connection is ready.
+     * @param event Ready
+     * @param listener Name Socket data
+     */
+    on(event: "ready", listener: (name: string) => void): this;
+
+    /**
+     * Emitted when a new track is playing at LISTEN.moe.
+     * @param event SongUpdate
+     * @param listener Song data
+     */
+    on( event: "songUpdate", listener: (name: string, data: SongUpdateData) => void): this;
   }
 }
